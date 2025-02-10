741327b6cf312f93cd06b5fab1bccc9487d6b025
Amirul Hossain <amirul1709@hotmail.com>
2025-02-10 14:29:00 -0700

    Added header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,10 @@ function App() {
     <main>
       <header>
         <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
       </header>
     </main>
   );
